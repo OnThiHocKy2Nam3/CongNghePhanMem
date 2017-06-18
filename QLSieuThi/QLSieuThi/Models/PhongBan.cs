2c4135bd831221d177cd625a41a2d2b4e039ab67
doanthanhhuong <huongthanhlan@gmail.com>
2017-06-18 10:56:16 +0700

    chuong trinh

---
+++ QLSieuThi/QLSieuThi/Models/PhongBan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLSieuThi.Models
+{
+    class PhongBan
+    {
+        public PhongBan() { }
+        public String Ma { get; set; }
+        public String Ten { get; set; }
+        public String DiaChi { get; set; }
+        public String SoDienThoai { get; set; }
+        public String Email { get; set; }
+    }
+}
